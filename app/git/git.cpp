ce2c3632d1d0c8dfe5e770ee8fc377eb9962e685
Michael <michael@unetresgrossebite.com>
2018-06-02 13:52:45 +0200

    adds libgit2

---
+++ app/git/git.cpp
@@ -0,0 +1,102 @@
+#include "git.hpp"
+#include <git2.h>
+#include <sstream>
+
+using namespace Git;
+using namespace std;
+
+GlobalState::GlobalState()  { git_libgit2_init(); }
+GlobalState::~GlobalState() { git_libgit2_shutdown(); }
+
+//
+// Repository
+//
+Repository::~Repository()
+{
+  if (handle != nullptr)
+    git_repository_free(handle);
+}
+
+void Repository::clone(const string& url, const string& path)
+{
+  int error = git_clone(&handle, url.c_str(), path.c_str(), nullptr);
+
+  if (error < 0)
+    throw Git::Exception(error);
+}
+
+void Repository::open(const string& path)
+{
+  int error = git_repository_open(&handle, path.c_str());
+
+  if (error < 0)
+    throw Git::Exception(error);
+}
+
+std::shared_ptr<Remote> Repository::find_remote(const string& name)
+{
+  return std::make_shared<Remote>(handle, name);
+}
+
+void Repository::checkout(const string& name, int strategy)
+{
+  git_checkout_options opts = GIT_CHECKOUT_OPTIONS_INIT;
+  Git::Object treeish;
+  int error;
+
+  opts.checkout_strategy = strategy;
+  error = git_revparse_single(&(treeish.handle), handle, name.c_str());
+  if (error < 0) throw Git::Exception(error);
+  error = git_checkout_tree(handle, treeish.handle, &opts);
+  if (error < 0) throw Git::Exception(error);
+}
+
+//
+// Object
+//
+Object::Object() {}
+
+Object::~Object()
+{
+  if (handle)
+    git_object_free(handle);
+}
+
+//
+// Remote
+//
+Remote::Remote(git_repository* repository_handle, const string& name)
+{
+  int error = git_remote_lookup(&handle, repository_handle, name.c_str());
+
+  if (error < 0)
+    throw Git::Exception(error);
+}
+
+Remote::~Remote()
+{
+  if (handle != nullptr)
+    git_remote_free(handle);
+}
+
+void Remote::pull()
+{
+  fetch("pull");
+}
+
+void Remote::fetch(const string& operation)
+{
+  int error = git_remote_fetch(handle, NULL, NULL, operation.c_str());
+
+  if (error < 0)
+    throw Git::Exception(error);
+}
+
+Exception::Exception(int code)
+{
+  const git_error* e = giterr_last();
+  std::stringstream stream;
+
+  stream << "git error: " << code << '/' << e->klass << ": " << e->message;
+  message = stream.str();
+}
---
+++ app/git/git.hpp
@@ -0,0 +1,64 @@
+#ifndef  GIT_HPP
+# define GIT_HPP
+
+# include <string>
+# include <exception>
+# include <memory>
+# include <git2.h>
+
+namespace Git
+{
+  struct GlobalState
+  {
+    GlobalState();
+    ~GlobalState();
+  };
+
+  struct Object
+  {
+    Object();
+    ~Object();
+
+    git_object* handle = nullptr;
+  };
+
+  class Remote
+  {
+  public:
+   Remote(git_repository*, const std::string&);
+   ~Remote();
+
+   void fetch(const std::string& operation = "fetch");
+   void pull();
+
+  private:
+    git_remote* handle = nullptr;
+  };
+
+  class Repository
+  {
+  public:
+    ~Repository();
+
+    void clone(const std::string& url, const std::string& path);
+    void open(const std::string& path);
+
+    std::shared_ptr<Remote> find_remote(const std::string& name);
+    void checkout(const std::string& name, int strategy = GIT_CHECKOUT_SAFE);
+
+  private:
+    git_repository* handle = nullptr;
+  };
+
+  struct Exception : public std::exception
+  {
+    Exception(int code);
+
+    const char* what() const noexcept { return message.c_str(); }
+
+  private:
+    std::string message;
+  };
+}
+
+#endif
