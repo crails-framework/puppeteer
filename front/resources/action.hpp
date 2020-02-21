ab9bc82b82499af5fd198550d7b64c065c077cf4
Michael <michael@unetresgrossebite.com>
2020-02-21 22:58:30 +0100

    fix missing front/resources folder

---
+++ front/resources/action.hpp
@@ -0,0 +1,17 @@
+#ifndef  STRUCT_ACTION_HPP
+# define STRUCT_ACTION_HPP
+
+struct Action
+{
+  Action() {}
+  Action(const std::string& _icon, const std::string& _label, std::function<void ()> _callback) :
+    icon(_icon), label(_label), callback(_callback) {}
+
+  std::string            icon;
+  std::string            label;
+  std::function<void ()> callback;
+};
+
+typedef std::list<Action> Actions;
+
+#endif
---
+++ front/resources/menu_entry.hpp
@@ -0,0 +1,13 @@
+#ifndef  STRUCT_MENU_ENTRY_HPP
+# define STRUCT_MENU_ENTRY_HPP
+
+struct MenuEntry
+{
+  MenuEntry(const std::string& a, const std::string& b, const std::string& c) : icon(a), label(b), route(c) {}
+
+  std::string icon;
+  std::string label;
+  std::string route;
+};
+
+#endif
