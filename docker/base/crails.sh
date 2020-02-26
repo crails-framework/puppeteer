d91e90d73fd51876469781cb3a69d5b26195bdb9
Michael <michael@unetresgrossebite.com>
2020-02-26 14:00:18 +0100

    update crails version

--- docker/base/crails.sh
+++ docker/base/crails.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/Plaristote/crails.git
 cd crails
 
-git checkout 7da5224e9255c6d9599c2c5be12f1cd2d137db1a
+git checkout 2d88535607284d53e40b3b4eafa1cd4325f42cb3
 
 mkdir build
 cd build
