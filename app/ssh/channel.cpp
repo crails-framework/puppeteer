1772f756a2493b20d35151438a7e9b730f6b1a91
Michael <michael@unetresgrossebite.com>
2020-02-28 13:41:56 +0100

    fixed Ssh::Channel not handling ssh_channel_request_exec failure

--- app/ssh/channel.cpp
+++ app/ssh/channel.cpp
@@ -1,5 +1,7 @@
 #include "channel.hpp"
+#include <crails/logger.hpp>
 
+using namespace Crails;
 using namespace Ssh;
 using namespace std;
 
@@ -10,15 +12,21 @@ int Channel::exec(const string& command, Sync::Stream& output)
   int  bytes_red;
   int rc = ssh_channel_request_exec(handle, command.c_str());
 
-  while (!ssh_channel_is_eof(handle))
+  if (rc == SSH_OK)
   {
-    bytes_red = ssh_channel_read_timeout(handle, buffer, sizeof(buffer), 0, timeout_ms);
+    while (!ssh_channel_is_eof(handle))
+    {
+      bytes_red = ssh_channel_read_timeout(handle, buffer, sizeof(buffer), 0, timeout_ms);
 
-    for (int i = 0 ; i < bytes_red ; ++i)
-      output << buffer[i];
-    std::cout << buffer << std::endl;
+      for (int i = 0 ; i < bytes_red ; ++i)
+        output << buffer[i];
+      std::cout << buffer << std::endl;
+    }
+    return ssh_channel_get_exit_status(handle);
   }
-  return ssh_channel_get_exit_status(handle);
+  else
+    logger << Logger::Error << "Ssh::Channel: ssh_channel_request_exec returned with status " << rc << Logger::endl;
+  return -1;
 }
 
 Channel::~Channel()
