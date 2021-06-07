72fc5369adc03f117731443984df15c61d482721
Michael <michael@unetresgrossebite.com>
2021-06-07 16:26:06 +0200

    updates to app/ssh/channel that are probably wrong, but apparently work
    * previous version looks right, but does not work... anymore. It worked at
      some point. Not anymore.

--- app/ssh/channel.cpp
+++ app/ssh/channel.cpp
@@ -7,37 +7,39 @@ using namespace std;
 
 int Channel::exec(const string& command, Sync::Stream& output)
 {
-  bool is_eof         = false;
-  bool read_timed_out = false;
+  bool is_eof = false;
   char buffer[256];
   int  bytes_read;
   int rc = ssh_channel_request_exec(handle, command.c_str());
+  int foo = 0;
+
 
   if (rc == SSH_OK)
   {
-    is_eof = ssh_channel_is_eof(handle);
-    logger << Logger::Info;
-    logger << "Ssh::Channel ssh_channel_request_exec successfull" << Logger::endl;
-    logger << "Ssh::Channel running command `" << command << "` (timeout in " << timeout_ms << ')' << Logger::endl;
-    while (!is_eof && !read_timed_out)
+    while (ssh_channel_is_open(handle) && !(is_eof = ssh_channel_is_eof(handle)))
     {
-      switch (ssh_channel_poll_timeout(handle, timeout_ms, false)) {
-      case SSH_EOF:
-        is_eof = true;
-        logger << "Ssh::Channel command returned successfully" << Logger::endl;
-        break ;
-      case 0:
-        read_timed_out = true;
-        logger << "Ssh::Channel ssh_channel_read_timeout timed out" << Logger::endl;
-        break ;
-      default:
-        {
-          bytes_read = ssh_channel_read_timeout(handle, buffer, sizeof(buffer), 0, timeout_ms);
-          for (int i = 0 ; i < bytes_read ; ++i)
-            output << buffer[i];
-          is_eof = bytes_read == 0 || ssh_channel_is_eof(handle);
-        }
-        break ;
+      bytes_read = ssh_channel_read_timeout(handle, buffer, sizeof(buffer), 0, 600000 /*timeout_ms*/);
+      if (bytes_read < 0)
+      {
+        foo++;
+        logger << Logger::Error << "Ssh::Channel: ssh_channel_read_timeout returned: " << bytes_read << Logger::endl;
+	is_eof = ssh_channel_is_eof(handle);
+	if (foo > 1000)
+          return SSH_ERROR;
+      }
+      if (bytes_read > 0)
+      {
+        foo = 0;
+        for (int i = 0 ; i < bytes_read ; ++i)
+          output << buffer[i];
+      }
+      else
+      {
+        foo++;
+        logger << Logger::Error << "Ssh::Channel: ssh_channel_read_timeout timed out" << Logger::endl;
+	is_eof = ssh_channel_is_eof(handle);
+	if (foo > 1000)
+          break ;
       }
     }
   }
